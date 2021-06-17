a042c7737d57e00203453b9f8c67e6c5e3777c46
Marshall Eldridge <l2marshall@gmail.com>
2021-06-17 13:57:57 -0600

    WebView complete.

--- app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
@@ -13,6 +13,7 @@ import edu.cnm.deepdive.animals.model.Animal;
 import edu.cnm.deepdive.animals.service.WebServiceProxy;
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
@@ -52,7 +53,15 @@ public class MainActivity extends AppCompatActivity {
             .getAnimals(BuildConfig.API_KEY)
             .execute();
         if (response.isSuccessful()) {
-          Log.d(getClass().getName(), response.message());
+          List<Animal> animals = response.body();
+          Random rng = new Random();
+          String url = animals.get(rng.nextInt(animals.size())).getImageUrl();
+          runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+              contentView.loadUrl(url);
+            }
+          });
         } else {
           Log.e(getClass().getName(), response.message());
         }
