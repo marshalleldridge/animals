a05fa18a987503469f3f7b7976b59b0264731a47
Marshall Eldridge <l2marshall@gmail.com>
2021-06-17 10:58:48 -0600

    WebView in process.

--- app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
@@ -1,6 +1,10 @@
 package edu.cnm.deepdive.animals.controller;
 
 import android.util.Log;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import edu.cnm.deepdive.animals.BuildConfig;
@@ -13,10 +17,29 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+  private WebView contentView;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    contentView = findViewById(R.id.content_view);
+    setUpWebView();
+  }
+
+  private void setUpWebView(){
+    contentView.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        return false;
+      }
+    });
+    WebSettings settings = contentView.getSettings();
+    settings.setSupportZoom(true);
+    settings.setBuiltInZoomControls(true);
+    settings.setDisplayZoomControls(false);
+    settings.setUseWideViewPort(true);
+    settings.setLoadWithOverviewMode(true);
     new Retriever().start();
   }
 
